a36357dc9ce0cb1ac1d06436df953a92c3a51c9c
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-01-16 23:10:54 +0600

    added navbar git push

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,11 @@
 import Banner from "@/components/Home/Banner";
+import Nav from "@/components/Shared/Nav";
 
 
 export default function Home() {
   return (
     <main className="">
+      <Nav></Nav>
       <Banner></Banner>
     </main>
   )
--- components/Home/Banner.tsx
+++ components/Home/Banner.tsx
@@ -4,15 +4,15 @@ import Container from "../Shared/Container";
 
 const Banner = () => {
     return (
-        <div className="">
+        <div className="bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black">
             <Container>
-                <div className="text-white flex flex-col-reverse md:flex-row items-center justify-center h-screen">
-                    <div className="max-w-5xl mx-auto text-left md:text-center md:order-1">
+                <div className="text-white flex flex-col md:flex-row items-center justify-center h-screen">
+                    <div className="max-w-5xl mx-auto text-left ">
                         <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold mb-4">We Are Massive Growth</h1>
                         <h2 className="text-lg md:text-xl lg:text-2xl mb-6">Helping You Grow Your Agency with Web Development Services</h2>
                         <button className="btn bg-[#00eeff] py-2 px-6 md:px-10 font-bold rounded-lg text-black mb-8">Contact Now</button>
                     </div>
-                    <div className="md:order-0">
+                    <div className="order-0 md:order-1 mt-8 md:mt-0 md:ml-8 lg:w-[800px]">
                         <Image
                             src={CoverImage}
                             layout="responsive"
