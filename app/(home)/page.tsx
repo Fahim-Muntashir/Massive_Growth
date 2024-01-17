bc68a2f0a6a702bc17ff05dc0b012c2af7493cb6
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-01-17 23:17:45 +0600

    add full home page without added content

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,10 @@
 import About from "@/components/Home/About";
 import Banner from "@/components/Home/Banner";
+import { Faq } from "@/components/Home/Faq";
 import Marque from "@/components/Home/Marque";
+import Contact from "@/components/Home/Contact";
+import { Team } from "@/components/Home/Team";
+import { Services } from "@/components/Home/Services";
 
 
 export default function Home() {
@@ -8,7 +12,11 @@ export default function Home() {
     <main className="">
       <Banner></Banner>
       <Marque></Marque>
+      <Services></Services>
       <About></About>
+      <Team></Team>
+      <Faq></Faq>
+      <Contact></Contact>
     </main>
   )
 }
