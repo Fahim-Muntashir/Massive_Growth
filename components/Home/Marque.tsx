d0f836d1c70e97094d444331b5c558c87ff6a19f
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-01-20 23:17:31 +0600

    added for deploy

--- components/Home/Marque.tsx
+++ components/Home/Marque.tsx
@@ -3,21 +3,21 @@ import React from 'react';
 const Marque = () => {
     return (
         <div>
-            <div class="relative flex bg-pink-600 overflow-x-hidden">
-                <div class="py-2 animate-marquee whitespace-nowrap">
-                    <span class="text-4xl mx-4">Marquee Item 1</span>
-                    <span class="text-4xl mx-4">Marquee Item 2</span>
-                    <span class="text-4xl mx-4">Marquee Item 3</span>
-                    <span class="text-4xl mx-4">Marquee Item 4</span>
-                    <span class="text-4xl mx-4">Marquee Item 5</span>
+            <div className="relative flex bg-pink-600 overflow-x-hidden">
+                <div className="py-2 animate-marquee whitespace-nowrap">
+                    <span className="text-4xl mx-4">Marquee Item 1</span>
+                    <span className="text-4xl mx-4">Marquee Item 2</span>
+                    <span className="text-4xl mx-4">Marquee Item 3</span>
+                    <span className="text-4xl mx-4">Marquee Item 4</span>
+                    <span className="text-4xl mx-4">Marquee Item 5</span>
                 </div>
 
-                <div class="absolute top-0 py-2 animate-marquee2 whitespace-nowrap">
-                    <span class="text-4xl mx-4">Marquee Item 1</span>
-                    <span class="text-4xl mx-4">Marquee Item 2</span>
-                    <span class="text-4xl mx-4">Marquee Item 3</span>
-                    <span class="text-4xl mx-4">Marquee Item 4</span>
-                    <span class="text-4xl mx-4">Marquee Item 5</span>
+                <div className="absolute top-0 py-2 animate-marquee2 whitespace-nowrap">
+                    <span className="text-4xl mx-4">Marquee Item 1</span>
+                    <span className="text-4xl mx-4">Marquee Item 2</span>
+                    <span className="text-4xl mx-4">Marquee Item 3</span>
+                    <span className="text-4xl mx-4">Marquee Item 4</span>
+                    <span className="text-4xl mx-4">Marquee Item 5</span>
                 </div>
             </div>
 
